d4946045ba2161f2daa932a55d82764c03292bd0
ckuryl <155270699+ckury@users.noreply.github.com>
2024-11-24 17:22:36 -0500

    Code Blocks

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
 const banner = document.createElement('template');
-
 banner.innerHTML = `
     <div class="banner"></div>
 `;
 
 document.body.prepend(banner.content);
+
+const sidenav = document.createElement('template');
+sidenav.innerHTML = `
+    <div class="sidenav">
+        <p>ckury.github.io</p>
+        <a href="index.html">Hello World</a>
+    </div>
+`;
+
+document.body.prepend(sidenav.content);

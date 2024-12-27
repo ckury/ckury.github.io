e238b4aff3d55a5f25731dd71306aa1dd847e197
ckuryl <155270699+ckury@users.noreply.github.com>
2024-12-27 13:53:20 -0500

    Added version numbers and created home page

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+// FW-version: 1.0
+
 const banner = document.createElement('template');
 banner.innerHTML = `
     <div class="banner"></div>
@@ -8,8 +10,8 @@ document.body.prepend(banner.content);
 const sidenav = document.createElement('template');
 sidenav.innerHTML = `
     <div class="sidenav">
-        <a href="/" class="sidetitle">ckury.github.io</a>
-        <a href="/index.html" class="sideoption">Hello World</a>
+        <a href="/" class="sidetitle">CKURY GitHub</a>
+        <a href="/hello_world.html" class="sideoption">Hello World</a>
         <a href="/about.html" class="sideoption">About</a>
     </div>
 `;

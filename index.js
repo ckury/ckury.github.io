c7fb6138d20fbbb48dc064050ab3f1bac9f618a5
ckuryl <155270699+ckury@users.noreply.github.com>
2024-12-29 14:21:22 -0500

    Working sidenav

--- index.js
+++ index.js
@@ -10,10 +10,24 @@ document.body.prepend(banner.content);
 const sidenav = document.createElement('template');
 sidenav.innerHTML = `
     <div class="sidenav">
-        <a href="/" class="sidetitle">CKURY GitHub</a>
-        <a href="/pages/hello_world.html" class="sideoption">Hello World</a>
-        <a href="/pages/about.html" class="sideoption">About</a>
+        <a href="/pages/hello_world.html">Hello World</a>
+        <a href="/pages/about.html">About</a>
+        <button class="dropdown-btn" onclick="toggleDropdown(this)">STM32</button>
+        <div class="dropdown-container">
+            <a href="#">Product 1</a>
+            <a href="#">Product 2</a>
+            <a href="#">Product 3</a>
+        </div>
     </div>
 `;
 
 document.body.prepend(sidenav.content);
+
+function toggleDropdown(btn) {
+    var dropdownContent = btn.nextElementSibling; 
+    if (dropdownContent.style.display === "block") {
+      dropdownContent.style.display = "none";
+    } else {
+      dropdownContent.style.display = "block";
+    }
+  }
